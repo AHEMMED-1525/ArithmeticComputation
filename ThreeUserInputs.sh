8a2a3e7972eb36902fa1559718fd3de026728d94
Ahemmed <68282883+AHEMMED-1525@users.noreply.github.com>
2020-08-28 20:09:31 +0530

    Delete ThreeUserInputs.sh

--- ThreeUserInputs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-#Taking the Three inputs from the user
-read -p "Enter the first Input " Firstuser
-read -p "Enter the first Input " Seconduser
-read -p "Enter the first Input " Thirduser
