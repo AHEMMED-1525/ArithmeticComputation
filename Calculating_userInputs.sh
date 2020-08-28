476f668ec0b0290855627be055fe1d13690c2ed4
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-08-28 19:58:52 +0530

    Applied Arithmeticoperations

---
+++ Calculating_userInputs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+#Taking the Three inputs from the user
+read -p "Enter the first Input " Firstuser
+read -p "Enter the first Input " Seconduser
+read -p "Enter the first Input " Thirduser
+
+a=$Firstuser
+b=$Seconduser
+c=$Thirduser
+
+result=$((a+b*c))
